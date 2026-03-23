cc0cf8dd45e5fce157cd02eb8fad8e828c9f4740
Anastasia Solodova <132252079+nastiasolo@users.noreply.github.com>
2026-03-23 15:13:26 +0700

    fetched currencies, styled input and select elements

--- courses/frontend/advanced-javascript/week-3/script.js
+++ courses/frontend/advanced-javascript/week-3/script.js
@@ -1,2 +1,30 @@
-let response = fetch("https://open.er-api.com/v6/latest/USD");
-console.log(response);
+const fromSelect = document.querySelector("#currency-from");
+const toSelect = document.querySelector("#currency-to");
+
+async function getCurrencies() {
+  try {
+    const response = await fetch("https://open.er-api.com/v6/latest/USD");
+    const data = await response.json();
+
+    const currencies = Object.keys(data.rates);
+
+    currencies.forEach((currency) => {
+      const optionFrom = document.createElement("option");
+      optionFrom.value = currency;
+      optionFrom.textContent = currency;
+      fromSelect.appendChild(optionFrom);
+
+      const optionTo = document.createElement("option");
+      optionTo.value = currency;
+      optionTo.textContent = currency;
+      toSelect.appendChild(optionTo);
+    });
+
+    fromSelect.value = "EUR";
+    toSelect.value = "DKK";
+  } catch (error) {
+    console.error("Error getting data: ", error);
+  }
+}
+
+getCurrencies();
